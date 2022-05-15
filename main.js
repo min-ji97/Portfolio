54827a0b71ed071c0ac35deba9578197a679889a
jominji <whalswl576@gmail.com>
2022-05-15 23:22:58 +0900

    (html+js) swiper

--- main.js
+++ main.js
@@ -4,10 +4,25 @@
  import 'swiper/css';
  import 'swiper/css/navigation';
  import 'swiper/css/pagination';
+import 'swiper/css/bundle';
 
  // init Swiper:
  const swiper = new Swiper('.swiper', {
    // configure Swiper to use modules
+   direction: 'vertical',
+   loop: true,
    modules: [Navigation, Pagination],
+  pagination: {
+    el : '.swiper-pagination'
+  },
+
+  navigation: {
+    nextEl: '.wiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar : {
+    el : '.swiper-scrollbar'
+  },
+
 
  });
