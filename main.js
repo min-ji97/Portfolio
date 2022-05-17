209bd01904acc59326160ca829f680cc3ab560d2
jominji <whalswl576@gmail.com>
2022-05-17 23:53:15 +0900

    에러...도저히 모르겠따..

--- main.js
+++ main.js
@@ -1,24 +1,38 @@
  // core version + navigation, pagination modules:
- import Swiper, { Navigation, Pagination } from 'swiper';
+//  import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper from './swiper.js';
+
+
  // import Swiper and modules styles
- import 'swiper/css';
- import 'swiper/css/navigation';
- import 'swiper/css/pagination';
-import 'swiper/css/bundle';
+import './swiper/swiper-bundle.css';
+
+//  import 'swiper/css';
+//  import 'swiper/css/navigation';
+//  import 'swiper/css/pagination';
+// import 'swiper/css/bundle';
+
+// npx http-server 서버 켜서 하기..!
+
+console.log('입력이 잘 되는가?!?!');
+
 
  // init Swiper:
- const swiper = new Swiper('.swiper', {
+  var swiper = new Swiper('.swiper', {
    // configure Swiper to use modules
-   direction: 'vertical',
-   loop: true,
-   modules: [Navigation, Pagination],
+  // direction: 'vertical',
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+
+  //  modules: [Navigation, Pagination],
   pagination: {
-    el : '.swiper-pagination'
+    el : '.swiper-pagination',
+    clickable: true,
   },
 
   navigation: {
-    nextEl: '.wiper-button-next',
     prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
   },
   scrollbar : {
     el : '.swiper-scrollbar'
